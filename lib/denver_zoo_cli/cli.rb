9073332ac5dc1a8b34d99ba7bce832f5e0b4ef6b
Dean Erickson <dmepilot@yahoo.com>
2020-07-30 20:25:34 +0000

    added scraper details

--- lib/denver_zoo_cli/cli.rb
+++ lib/denver_zoo_cli/cli.rb
@@ -3,22 +3,27 @@ class DenverZooCli::Cli
   def run 
     puts "Welcome to the Denver Zoo!"
     scrape_test
+  
   end
   
   def scrape_test
     site = "https://denverzoo.org/animals/"
     scrape = Nokogiri::HTML(open(site))
-    test = scrape.css("a.animals-list.ddd").first.attribute("href").value
+    test = scrape.css("a.animals-list.ddd").attribute("href").value
+    all_animal_sites = scrape.css("a.animals-list.ddd").collect {|x|  x.attribute("href").value}
+    first_site = all_animal_sites[0]
+    animal_classification = Nokogiri::HTML(open(first_site)).css("div.zoo-animal-classification").text
+    animal_habitat = Nokogiri::HTML(open(first_site)).css("div.fl-module-content fl-node-content").css("div.fl-rich-text").css("p").text
     binding.pry
   end
   
+  
 end
 
-# <div class="col-lg-4 col-md-6 m-b-30 m-b-10-sm">
-#                     <a href="https://denverzoo.org/animals/african-lion/" class="animals-list ddd">
-#                         <img class="img-fluid lazy loaded" src="https://denverzoo.org/wp-content/uploads/2018/09/south-african-lion_feature-350x350.jpg" data-src="https://denverzoo.org/wp-content/uploads/2018/09/south-african-lion_feature-350x350.jpg" alt="Animal" title="Animal" data-was-processed="true">
-#                         <div class="animals-list__title">
-#                             <h4> African Lion <span>Panthera leo melanochaita</span></h4>
-#                         </div>
-#                     </a>
-#                 </div>
+# <div class="fl-module-content fl-node-content">
+# 		<div class="fl-rich-text">
+# 	<p>African penguins, also called black-footed penguins, nest on sandy or rocky islands and mainland beaches with temperate climates, and they forage for food in cold ocean currents whose temperatures range from 42 to 68 degrees Fahrenheit (5-20° C).</p>
+# <p>This species lives only on coastal islands and the mainland of southwestern Africa from Namibia to South Africa.</p>
+# </div>
+# 	</div>
+
