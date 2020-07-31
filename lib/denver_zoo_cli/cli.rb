211abad2a456cb6cc36ce94c7817cc9f92545ce3
Dean Erickson <dmepilot@yahoo.com>
2020-07-31 01:42:09 +0000

    more scraper testing

--- lib/denver_zoo_cli/cli.rb
+++ lib/denver_zoo_cli/cli.rb
@@ -10,8 +10,8 @@ class DenverZooCli::Cli
     site = "https://denverzoo.org/animals/"
     scrape = Nokogiri::HTML(open(site))
     test = scrape.css("a.animals-list.ddd").attribute("href").value
-    all_animal_sites = scrape.css("a.animals-list.ddd").collect {|x|  x.attribute("href").value}  #70 sites (elements 0-69)
-    first_site = all_animal_sites[69]
+    all_animal_sites = scrape.css("a.animals-list.ddd").collect {|x|  x.attribute("href").value}  #70 animal sites (elements 0-69)
+    first_site = all_animal_sites[25]
     animal_classification = Nokogiri::HTML(open(first_site)).css("div.zoo-animal-classification").text
     rough_text = Nokogiri::HTML(open(first_site)).css("p").text
     x = Nokogiri::HTML(open(first_site)).css(".fl-module.fl-module-rich-text.fl-node-5b9808434ad12").css("div.fl-rich-text").css("p").text
@@ -22,6 +22,10 @@ class DenverZooCli::Cli
     animal_family = y[3].text
     animal_genus = y[4].text
     animal_species = y[5].text
+    animal_range = y[6].text
+    animal_habitat = y[7].text
+    #animal_fun_facts = y[8].text
+    
     binding.pry
   end
   
