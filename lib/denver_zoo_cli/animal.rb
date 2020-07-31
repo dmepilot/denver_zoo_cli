156d45ed8e897a5ff0730a6d12f99b39f7539a81
Dean Erickson <dmepilot@yahoo.com>
2020-07-31 15:16:40 +0000

    scraper

---
+++ lib/denver_zoo_cli/animal.rb
@@ -0,0 +1,9 @@
+class Animal
+  
+  attr_accessor :name, :klass, :order, :family, :genus, :species
+  
+  def initialize 
+    
+  end
+  
+end
--- lib/denver_zoo_cli/cli.rb
+++ lib/denver_zoo_cli/cli.rb
@@ -1,21 +1,33 @@
 class DenverZooCli::Cli
   
+  attr_accessor :all_animal_sites, :name
+  
   def run 
     puts "Welcome to the Denver Zoo!"
     scrape_test
+    #list_choices(all_animal_sites)
+  end
   
+  def initialize
+    site = "https://denverzoo.org/animals/"
+    scrape = Nokogiri::HTML(open(site))
+    test = scrape.css("a.animals-list.ddd").attribute("href").value
+    @all_animal_sites = scrape.css("a.animals-list.ddd").collect {|x|  x.attribute("href").value}  #70 animal sites (elements 0-69)
+    
   end
   
   def scrape_test
     site = "https://denverzoo.org/animals/"
     scrape = Nokogiri::HTML(open(site))
     test = scrape.css("a.animals-list.ddd").attribute("href").value
-    all_animal_sites = scrape.css("a.animals-list.ddd").collect {|x|  x.attribute("href").value}  #70 animal sites (elements 0-69)
-    first_site = all_animal_sites[25]
+    @all_animal_sites = scrape.css("a.animals-list.ddd").collect {|x|  x.attribute("href").value}  #70 animal sites (elements 0-69)
+    first_site = all_animal_sites[0]
     animal_classification = Nokogiri::HTML(open(first_site)).css("div.zoo-animal-classification").text
     rough_text = Nokogiri::HTML(open(first_site)).css("p").text
     x = Nokogiri::HTML(open(first_site)).css(".fl-module.fl-module-rich-text.fl-node-5b9808434ad12").css("div.fl-rich-text").css("p").text
     y = Nokogiri::HTML(open(first_site)).css(".fl-module").css("div.fl-rich-text").css("p")
+    z = Nokogiri::HTML(open(first_site)).css("div.fl-rich-text ul li")[0].text #adaptations
+    w = Nokogiri::HTML(open(first_site)).css("div.fl-rich-text p").text
     array = y.size
     animal_klass = y[1].text
     animal_order = y[2].text
@@ -25,17 +37,18 @@ class DenverZooCli::Cli
     animal_range = y[6].text
     animal_habitat = y[7].text
     #animal_fun_facts = y[8].text
+    name = Nokogiri::HTML(open(first_site)).css("h1 span.fl-heading-text").text
     
     binding.pry
+  end
+  
+  def list_choices(name)
+    name.each.with_index(1) do |value, index|
+      puts "#{index}. #{value}"
+    end
   end
   
   
 end
 
-# <div class="fl-module-content fl-node-content">
-# 		<div class="fl-rich-text">
-# 	<p>African lions are typically found in savannas, plains, grasslands, dense bush and open woodlands where prey is abundant.&nbsp; They can also be found in smaller numbers in semidesert and mountain habitats.</p>
-# <p>This species can be found in most countries of sub-Saharan Africa, primarily inhabiting eastern and southern Africa up to elevations of 13,700 ft. (4,200 m).</p>
-# </div>
-# 	</div>
 
