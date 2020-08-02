2b740c17b181d35ce33fb9bbd9639fd063f8380c
Dean Erickson <dmepilot@yahoo.com>
2020-08-02 18:38:21 +0000

    cleanup

--- lib/denver_zoo_cli/scraper.rb
+++ lib/denver_zoo_cli/scraper.rb
@@ -2,9 +2,9 @@ class DenverZooCli::Scraper
   
     @@all_animal_sites = []
     
-     def self.get_sites
+    def self.get_sites
        @@all_animal_sites = Nokogiri::HTML(open("https://denverzoo.org/animals/")).css("a.animals-list.ddd").collect {|x|  x.attribute("href").value}
-     end
+    end
     
     def self.get_names
         names=@@all_animal_sites.collect do |y|
@@ -14,7 +14,6 @@ class DenverZooCli::Scraper
     end
   
     def self.get_animal_data(name, url)
-      
       new_animal=DenverZooCli::Animal.new(name, url)
       doc = url
       scientific_data=Nokogiri::HTML(open(url)).css(".fl-module").css("div.fl-rich-text").css("p")
@@ -28,32 +27,6 @@ class DenverZooCli::Scraper
       animal_fun_facts = Nokogiri::HTML(open(url)).css("div.fl-rich-text ul li")
       c=animal_fun_facts.size
       animal_fun_facts.each {|fact| new_animal.fun_facts << fact.text}
-      
-      
     end
-  
-  
-  
-  
-   def self.scrape_test
-    site = "https://denverzoo.org/animals/"
-    scrape = Nokogiri::HTML(open(site))
-    test = scrape.css("a.animals-list.ddd").attribute("href").value
-    first_site = @all_animal_sites[0]
-    x = Nokogiri::HTML(open(first_site)).css(".fl-module.fl-module-rich-text.fl-node-5b9808434ad12").css("div.fl-rich-text").css("p").text
-    y = Nokogiri::HTML(open(first_site)).css(".fl-module").css("div.fl-rich-text").css("p")
-    z = Nokogiri::HTML(open(first_site)).css("div.fl-rich-text ul li")[0].text #adaptations
-    w = Nokogiri::HTML(open(first_site)).css("div.fl-rich-text p").text
-   
-    #animal_fun_facts = y[8].text
-    
-    
-  end
-  
-
-  
-
-  
-  
   
 end
