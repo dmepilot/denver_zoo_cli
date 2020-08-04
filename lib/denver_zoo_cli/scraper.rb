e635fa02c099fbfecb47bc1b6725a06c98e76b37
Dean Erickson <dmepilot@yahoo.com>
2020-08-04 01:33:43 +0000

    classification WIP

--- lib/denver_zoo_cli/scraper.rb
+++ lib/denver_zoo_cli/scraper.rb
@@ -25,8 +25,8 @@ class DenverZooCli::Scraper
       new_animal.genus = classification.find{|c| c[/Genus/]}.lstrip unless !classification.find{|c| c[/Genus/]}
       new_animal.species = classification.find{|c| c[/Species/]}.lstrip unless !classification.find{|c| c[/Species/]}
       new_animal.subspecies = classification.find{|c| c[/Subspecies/]}.lstrip unless !classification.find{|c| c[/Subspecies/]}
-      #works for grizzly and most others. not cheetah 
-      if classification.find{|c| c[/Subspecies/]} || classification.find{|c| c[/Class/]} != classification[0]
+      #works for grizzly and cheetah but not most others
+      if classification.find{|c| c[/Subspecies/]} || classification.find{|c| c[/Class/]} != classification[0] || classification.find{|c| c[/Class/]} != scientific_data[0]
         new_animal.habitat = Nokogiri::HTML(open(url)).css(".fl-rich-text").text.split("\t")[3].split("\n")[0]
         new_animal.range = Nokogiri::HTML(open(url)).css(".fl-rich-text").text.split("\t")[3].split("\n")[1]
       else
